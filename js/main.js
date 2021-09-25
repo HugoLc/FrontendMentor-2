d7b35f5b751bf152c4beb7753d4ba734d26b225b
Hugo LC <hugolindoso.c@gmail.com>
2021-09-25 15:46:57 -0300

    Criado texto do info card

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ import InfoCard from '../js/info-card.js'
 const infoCardParagrafo = `
     Página criada atendendo as especificações do desafio proposto 
     no site Frontend Mentor. Praticado posicionamento de elementos,
-    CSS grid e flex, inserção de SVG e link para utilização de ícones 
-    em página externa.
+    CSS grid e flex, inserção de SVG, link para utilização de ícones 
+    em página externa e responsividade.
 `
 var infoCard = new InfoCard(infoCardParagrafo);
 
